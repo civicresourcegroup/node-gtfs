c77d98376942a3fa7eafaa82a28ac8f92562023a
vitaliik <vitaliik@civicresource.com>
2016-12-29 13:35:32 -0800

    add getAllRouteIds

---
+++ test/mocha/gtfs.getAllRouteIds.js
@@ -0,0 +1,63 @@
+const async = require('async');
+const path = require('path');
+const should = require('should');
+
+// libraries
+const config = require('../config.json');
+const gtfs = require('../../');
+
+
+// test support
+const database = require('../support/database');
+
+// setup fixtures
+var agenciesFixtures = [{
+  agency_key: 'caltrain',
+  path: path.join(__dirname, '../fixture/caltrain_20160406.zip')
+}];
+
+config.agencies = agenciesFixtures;
+
+describe('gtfs.getAllRouteIds(): ', () => {
+
+  before((done) => {
+    database.connect(config, done);
+  });
+
+  after((done) => {
+    async.series({
+      teardownDatabase: (next) => {
+        database.teardown(next);
+      },
+      closeDb: (next) => {
+        database.close(next);
+      }
+    }, done);
+  });
+
+  beforeEach((done) => {
+    async.series({
+      teardownDatabase: (next) => {
+        database.teardown(next);
+      },
+      executeDownloadScript: (next) => {
+        gtfs.import(config, next);
+      }
+    }, done);
+  });
+
+  it('should return route ids', (done) => {
+    gtfs.getAllRouteIds((err, res) => {
+      res = res.map((i) => i.route_id);
+      let randomIndex = 0;
+
+      should.not.exist(err);
+      should.exist(res);
+      res.should.be.Array();
+
+      randomIndex = Math.floor(Math.random() * res.length);
+      res[randomIndex].should.be.String();
+    });
+    done();
+  });
+});
