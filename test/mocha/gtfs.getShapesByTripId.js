d31dbc38bf986cf4ceb21bcfcc2ebafe892e00d3
vitaliik <vitaliik@civicresource.com>
2016-12-29 16:45:15 -0800

    add get Shape by Trip id

---
+++ test/mocha/gtfs.getShapesByTripId.js
@@ -0,0 +1,59 @@
+
+const async = require('async');
+const path = require('path');
+const should = require('should');
+
+// libraries
+const config = require('../config.json');
+const gtfs = require('../../');
+
+
+// test support
+const database = require('../support/database');
+
+// setup fixtures
+const agenciesFixtures = [{
+  agency_key: 'caltrain',
+  path: path.join(__dirname, '../fixture/caltrain_20160406.zip')
+}];
+
+config.agencies = agenciesFixtures;
+
+describe.only('gtfs.getShapesByTripId(): ', () => {
+  before((done) => {
+    database.connect(config, done);
+  });
+
+  after((done) => {
+    async.series({
+      teardownDatabase: (next) => {
+        database.teardown(next);
+      },
+      closeDb: (next) => {
+        database.close(next);
+      }
+    }, done);
+  });
+
+  beforeEach((done) => {
+    async.series({
+      teardownDatabase: (next) => {
+        database.teardown(next);
+      },
+      executeDownloadScript: (next) => {
+        gtfs.import(config, next);
+      }
+    }, done);
+  });
+
+  it('should return shape for the trip_id specified', (done) => {
+    const tripId = '23a';
+    gtfs.getTripById(agenciesFixtures[0].agency_key, tripId, (err,res) => {
+      const expectedShapeId = res.shape_id;
+      gtfs.getShapeByTripId(agenciesFixtures[0].agency_key, tripId, (er, resp) => {
+        resp.should.matchEach((i) => i.should.have.properties({'shape_id': expectedShapeId}));
+      });
+    });
+    done();
+  });
+});
---
+++ test/mocha/gtfs.getTripById.js
@@ -0,0 +1,57 @@
+
+
+const async = require('async');
+const path = require('path');
+const should = require('should');
+
+// libraries
+const config = require('../config.json');
+const gtfs = require('../../');
+
+
+// test support
+const database = require('../support/database');
+
+// setup fixtures
+const agenciesFixtures = [{
+  agency_key: 'caltrain',
+  path: path.join(__dirname, '../fixture/caltrain_20160406.zip')
+}];
+
+config.agencies = agenciesFixtures;
+
+describe.only('gtfs.getTripById(): ', () => {
+  before((done) => {
+    database.connect(config, done);
+  });
+
+  after((done) => {
+    async.series({
+      teardownDatabase: (next) => {
+        database.teardown(next);
+      },
+      closeDb: (next) => {
+        database.close(next);
+      }
+    }, done);
+  });
+
+  beforeEach((done) => {
+    async.series({
+      teardownDatabase: (next) => {
+        database.teardown(next);
+      },
+      executeDownloadScript: (next) => {
+        gtfs.import(config, next);
+      }
+    }, done);
+  });
+
+  it('should return trip for the trip_id specified', (done) => {
+    const tripId = '26a';
+    gtfs.getTripById(agenciesFixtures[0].agency_key, tripId, (err, res) => {
+      res.should.have.properties({'trip_id':tripId});
+    });
+    done();
+  });
+});
